c501dba2ce2f377b842d83156d6f62b4e0e0a35d
SapnaCourse5i <sapna.verma@course5i.com>
2023-09-13 15:34:03 +0530

    xgboost

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ LOCAL_REQUIREMENTS = [
     "delta-spark==1.1.0",
     "scikit-learn",
     "pandas",
-    "mlflow"
-    "xgboost"
-    "lightgm",
+    "mlflow",
+    "xgboost",
+    "lightgm"
 ]
 
 TEST_REQUIREMENTS = [
