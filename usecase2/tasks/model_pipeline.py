48e67f9b0fa302f8436ace0c0d403eebd72dc6af
SapnaCourse5i <140520534+SapnaCourse5i@users.noreply.github.com>
2023-09-21 18:37:18 +0530

    module

--- usecase2/tasks/model_pipeline.py
+++ usecase2/tasks/model_pipeline.py
@@ -42,8 +42,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
 from databricks.feature_store import feature_table, FeatureLookup
-from usecase2 import utils
-from utils import select_kbest_features
+from usecase2.utils import select_kbest_features
+# from utils import select_kbest_features
 
 # from evidently import ColumnMapping
 
