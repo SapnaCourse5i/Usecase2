891d755f00d24631efb8a497f404067b61b8ad57
SapnaCourse5i <sapna.verma@course5i.com>
2023-10-17 11:39:57 +0530

    2

--- usecase2/tasks/model_pipeline.py
+++ usecase2/tasks/model_pipeline.py
@@ -85,10 +85,10 @@ class model_training(Task):
         # fs.create_training_set looks up features in model_feature_lookups that match the primary key from inference_data_df
         training_set = fs.create_training_set(inference_data_df, model_feature_lookups, label=target,exclude_columns=lookup_key)
         df= training_set.load_df().toPandas()
-        # df1=df.drop(self.conf['features']['target_col'],axis=1)
+        df1=df.drop(self.conf['features']['target_col'],axis=1)
         # print(df1.columns)
-        # top_features=select_kbest_features(df1,df[self.conf['features']['target_col']],n=self.conf['kbestfeatures']['no_of_features'])
-        # df=df[top_features+ [self.conf['features']['target_col']]]
+        top_features=select_kbest_features(df1,df[self.conf['features']['target_col']],n=self.conf['kbestfeatures']['no_of_features'])
+        df=df[top_features+ [self.conf['features']['target_col']]]
         print(df.columns)
         # X_train, X_val, y_train, y_val,X_test,y_test=self.train_test_val_split(df_input,test_split,val_split)
         
@@ -366,7 +366,7 @@ class model_training(Task):
       
          X_test1=fs.read_table(self.conf['feature-store']['table_name'])
          
-       
+         X_test1=X_test1.select('HCP_ID','NPI_ID')
          print('scoring now')
          test_pred = fs.score_batch("models:/usecase2_model/latest", X_test1)
          print('scoring done')
